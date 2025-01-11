b99919c8204867c3441fd5f894551e7210a54d1f
Dvis <gokhaledurgesh4@gmail.com>
2025-01-11 11:58:21 +0530

    Update image.py

--- strings/image.py
+++ strings/image.py
@@ -1,12 +1,4 @@
 image = [
-"https://files.catbox.moe/ciqfdq.jpg",
-"https://files.catbox.moe/jrfn5c.jpg",
-"https://files.catbox.moe/tb20q7.jpg",
-"https://files.catbox.moe/htuibk.jpg",
-"https://files.catbox.moe/rpghqf.jpg",
-"https://files.catbox.moe/70p4kt.jpg",
-"https://files.catbox.moe/cbr208.jpg",
-"https://files.catbox.moe/3fhcv2.jpg",
-"https://files.catbox.moe/3m0pth.jpg",
-"https://files.catbox.moe/pbekqf.jpg",
+"https://files.catbox.moe/6u5w5a.jpg",
+"https://files.catbox.moe/6u5w5a.jpg",
 ]
