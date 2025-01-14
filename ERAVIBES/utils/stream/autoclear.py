6afa22cf17398ef2f6a223c384b19db3c07aab49
Dvis <gokhaledurgesh4@gmail.com>
2025-01-14 13:36:33 +0530

    Update autoclear.py

--- ERAVIBES/utils/stream/autoclear.py
+++ ERAVIBES/utils/stream/autoclear.py
@@ -1,9 +1,8 @@
 import os
 import logging
-from config import autoclean  # Import the entire autoclean module
+from config import autoclean
 from ERAVIBES.utils.decorators import asyncify
 
-# Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -12,8 +11,8 @@ def auto_clean(popped):
     def _auto_clean(popped_item):
         try:
             rem = popped_item["file"]
-            autoclean.remove(rem)  # Use autoclean.remove instead of remove
-            cnt = autoclean.count(rem)  # Use autoclean.count instead of count
+            autoclean.remove(rem)
+            cnt = autoclean.count(rem)
             if cnt == 0:
                 if "vid_" not in rem and "live_" not in rem and "index_" not in rem:
                     try:
@@ -31,7 +30,3 @@ def auto_clean(popped):
             _auto_clean(pop)
     else:
         raise ValueError("Expected popped to be a dict or list.")
-
-# Example usage:
-# auto_clean({"file": "path/to/file.txt"})
-# auto_clean([{"file": "path/to/file1.txt"}, {"file": "path/to/file2.txt"}])
