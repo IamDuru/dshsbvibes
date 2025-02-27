9a566f401292b8953bc983309faebda1c80bfcdf
Dvis <gokhaledurgesh4@gmail.com>
2025-02-27 22:07:30 +0530

    Update stop.py

--- ERAVIBES/plugins/admins/stop.py
+++ ERAVIBES/plugins/admins/stop.py
@@ -22,6 +22,8 @@ async def stop_music(cli, message: Message, _, chat_id):
         return
     await ERA.stop_stream(chat_id)
     await set_loop(chat_id, 0)
-    await message.reply_text(
+    r = await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
+    await asyncio.sleep(3)
+    await r.delete()
