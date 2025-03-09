5322b014dfdcdb7ae2aac4373a5e7cd9db5c28bd
Dvis <gokhaledurgesh4@gmail.com>
2025-03-09 19:51:11 +0530

    Update Youtube.py

--- ERAVIBES/platforms/Youtube.py
+++ ERAVIBES/platforms/Youtube.py
@@ -1,4 +1,3 @@
-
 import asyncio
 import os
 import re
@@ -14,7 +13,6 @@ import config
 from ERAVIBES.utils.database import is_on_off
 from ERAVIBES.utils.formatters import time_to_seconds
 from ytdlx import ytdlx
-
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
@@ -32,7 +30,6 @@ async def shell_cmd(cmd):
         else:
             return errorz.decode("utf-8")
     return out.decode("utf-8")
-
 
 class YouTubeAPI:
     def __init__(self):
@@ -95,7 +92,7 @@ class YouTubeAPI:
                 duration_sec = 0
             else:
                 duration_sec = int(time_to_seconds(duration_min))
-        return title, duration_min, duration_sec, thumbnail, vidid
+            return title, duration_min, duration_sec, thumbnail, vidid
 
     async def title(
         self, link: str, videoid: Union[bool, str] = None
@@ -107,7 +104,7 @@ class YouTubeAPI:
         results = VideosSearch(link, limit=1)
         for result in (await results.next())["result"]:
             title = result["title"]
-        return title
+            return title
 
     async def duration(
         self, link: str, videoid: Union[bool, str] = None
@@ -119,7 +116,7 @@ class YouTubeAPI:
         results = VideosSearch(link, limit=1)
         for result in (await results.next())["result"]:
             duration = result["duration"]
-        return duration
+            return duration
 
     async def thumbnail(
         self, link: str, videoid: Union[bool, str] = None
@@ -131,7 +128,7 @@ class YouTubeAPI:
         results = VideosSearch(link, limit=1)
         for result in (await results.next())["result"]:
             thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
+            return thumbnail
 
     async def video(
         self, link: str, videoid: Union[bool, str] = None
@@ -186,14 +183,14 @@ class YouTubeAPI:
             vidid = result["id"]
             yturl = f"https://youtube.com{result['url_suffix']}"
             thumbnail = f"https://img.youtube.com/vi/{vidid}/hqdefault.jpg"
-        track_details = {
-            "title": title,
-            "link": yturl,
-            "vidid": vidid,
-            "duration_min": duration_min,
-            "thumb": thumbnail,
-        }
-        return track_details, vidid
+            track_details = {
+                "title": title,
+                "link": yturl,
+                "vidid": vidid,
+                "duration_min": duration_min,
+                "thumb": thumbnail,
+            }
+            return track_details, vidid
 
     async def formats(
         self, link: str, videoid: Union[bool, str] = None
@@ -231,7 +228,7 @@ class YouTubeAPI:
                             "yturl": link,
                         }
                     )
-        return formats_available, link
+            return formats_available, link
 
     async def slider(
         self,
@@ -266,119 +263,88 @@ class YouTubeAPI:
     ) -> str:
         if videoid:
             link = self.base + link
-        loop = asyncio.get_running_loop()
-
-        def audio_dl():
-            ydl_optssx = {
-                "format": "bestaudio/best",
-                "outtmpl": "downloads/%(id)s.%(ext)s",
-                "geo_bypass": True,
-                "nocheckcertificate": True,
-                "quiet": True,
-                "no_warnings": True,
-            }
-            x = yt_dlp.YoutubeDL(ydl_optssx)
-            info = x.extract_info(link, False)
-            xyz = os.path.join(
-                "downloads", f"{info['id']}.{info['ext']}"
-            )
-            if os.path.exists(xyz):
-                return xyz
-            x.download([link])
-            return xyz
-
-        def video_dl():
-            ydl_optssx = {
-                "format": "(bestvideo[height<=?720][width<=?1280][ext=mp4])+(bestaudio[ext=m4a])",
-                "outtmpl": "downloads/%(id)s.%(ext)s",
-                "geo_bypass": True,
-                "nocheckcertificate": True,
-                "quiet": True,
-                "no_warnings": True,
-            }
-            x = yt_dlp.YoutubeDL(ydl_optssx)
-            info = x.extract_info(link, False)
-            xyz = os.path.join(
-                "downloads", f"{info['id']}.{info['ext']}"
-            )
-            if os.path.exists(xyz):
-                return xyz
-            x.download([link])
-            return xyz
-
-        def song_video_dl():
-            formats = f"{format_id}+140"
-            fpath = f"downloads/{title}"
-            ydl_optssx = {
-                "format": formats,
-                "outtmpl": fpath,
-                "geo_bypass": True,
-                "nocheckcertificate": True,
-                "quiet": True,
-                "no_warnings": True,
-                "prefer_ffmpeg": True,
-                "merge_output_format": "mp4",
-            }
-            x = yt_dlp.YoutubeDL(ydl_optssx)
-            x.download([link])
-
-        def song_audio_dl():
-            fpath = f"downloads/{title}.%(ext)s"
-            ydl_optssx = {
-                "format": format_id,
-                "outtmpl": fpath,
-                "geo_bypass": True,
-                "nocheckcertificate": True,
-                "quiet": True,
-                "no_warnings": True,
-                "prefer_ffmpeg": True,
-                "postprocessors": [
-                    {
-                        "key": "FFmpegExtractAudio",
-                        "preferredcodec": "mp3",
-                        "preferredquality": "192",
-                    }
-                ],
-            }
-            x = yt_dlp.YoutubeDL(ydl_optssx)
-            x.download([link])
-
+        # Agar song video download karna ho toh purana method use karte hain
         if songvideo:
-            await loop.run_in_executor(None, song_video_dl)
+            def song_video_dl():
+                formats = f"{format_id}+140"
+                fpath = f"downloads/{title}"
+                ydl_optssx = {
+                    "format": formats,
+                    "outtmpl": fpath,
+                    "geo_bypass": True,
+                    "nocheckcertificate": True,
+                    "quiet": True,
+                    "no_warnings": True,
+                    "prefer_ffmpeg": True,
+                    "merge_output_format": "mp4",
+                }
+                x = yt_dlp.YoutubeDL(ydl_optssx)
+                x.download([link])
+            await asyncio.get_running_loop().run_in_executor(None, song_video_dl)
             fpath = f"downloads/{title}.mp4"
             return fpath
+
         elif songaudio:
-            await loop.run_in_executor(None, song_audio_dl)
+            def song_audio_dl():
+                fpath = f"downloads/{title}.%(ext)s"
+                ydl_optssx = {
+                    "format": format_id,
+                    "outtmpl": fpath,
+                    "geo_bypass": True,
+                    "nocheckcertificate": True,
+                    "quiet": True,
+                    "no_warnings": True,
+                    "prefer_ffmpeg": True,
+                    "postprocessors": [
+                        {
+                            "key": "FFmpegExtractAudio",
+                            "preferredcodec": "mp3",
+                            "preferredquality": "192",
+                        }
+                    ],
+                }
+                x = yt_dlp.YoutubeDL(ydl_optssx)
+                x.download([link])
+            await asyncio.get_running_loop().run_in_executor(None, song_audio_dl)
             fpath = f"downloads/{title}.mp3"
             return fpath
+
         elif video:
-            if await is_on_off(config.YTDOWNLOADER):
-                direct = True
-                downloaded_file = await loop.run_in_executor(
-                    None, video_dl
-                )
-            else:
-                proc = await asyncio.create_subprocess_exec(
-                    "yt-dlp",
-                    "-g",
-                    "-f",
-                    "best[height<=?720][width<=?1280]",
-                    f"{link}",
-                    stdout=asyncio.subprocess.PIPE,
-                    stderr=asyncio.subprocess.PIPE,
-                )
-                stdout, stderr = await proc.communicate()
-                if stdout:
-                    downloaded_file = stdout.decode().split("\n")[0]
-                    direct = None
-                else:
-                    return
-        else:
-            direct = True
-            downloaded_file = await loop.run_in_executor(
-                None, audio_dl
+            # Ab hum video ke liye direct streaming URL le rahe hain,
+            # jisse file download karne ka time na lage.
+            proc = await asyncio.create_subprocess_exec(
+                "yt-dlp",
+                "-g",
+                "-f",
+                "best[height<=?720][width<=?1280]",
+                link,
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
             )
-        return downloaded_file, direct
+            stdout, stderr = await proc.communicate()
+            if stdout:
+                direct_url = stdout.decode().split("\n")[0]
+                return direct_url, True
+            else:
+                return None
+        else:
+            # Audio ke liye direct streaming URL
+            proc = await asyncio.create_subprocess_exec(
+                "yt-dlp",
+                "-g",
+                "-f",
+                "bestaudio[ext=m4a]",
+                link,
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
+            )
+            stdout, stderr = await proc.communicate()
+            if stdout:
+                direct_url = stdout.decode().split("\n")[0]
+                return direct_url, True
+            else:
+                return None
+
     async def download(
         self,
         link: str,
