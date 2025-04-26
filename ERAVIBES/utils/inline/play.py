bdb49caaa3f24eb80ccb18f167df660a837773bb
Official Durgesh <durgeshgokhale656@gmail.com>
2025-04-26 10:54:52 +0530

    Update play.py

--- ERAVIBES/utils/inline/play.py
+++ ERAVIBES/utils/inline/play.py
@@ -157,6 +157,5 @@ def queue_markup(_, videoid, chat_id):
             InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
             InlineKeyboardButton(text="ᐅ", callback_data=f"ADMIN Resume|{chat_id}"),
-        ],
     ]
     return buttons
